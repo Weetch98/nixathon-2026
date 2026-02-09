e24b6709415712c47c82b78bef4cdc2017a96bb3
Attila Berczelly <attila.berczelly@nixs.com>
2026-02-09 15:28:50 +0100

    Initialized project, first commit

---
+++ src/main/java/me/beratta/nixathon/NixathonApplication.java
@@ -0,0 +1,13 @@
+package me.beratta.nixathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nixathon2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nixathon2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/beratta/nixathon/NixathonApplicationTests.java
@@ -0,0 +1,13 @@
+package me.beratta.nixathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nixathon2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
